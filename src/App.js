4be56fd6c5ef1bf272ac008431ed59943f3d076e
Weixiao <zhongwx@gmail.com>
2019-12-11 10:30:58 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
 import React from 'react';
 import logo from './logo.svg';
+import Datasheet from 'react-datasheet';
+import 'react-datasheet/lib/react-datasheet.css';
 import './App.css';
+import { AssertionError } from 'assert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor (props) {
+    const model = props.model;
+
+    const buildViewModel = function(m)
+    {
+      const prepareHeader = (axes, arr) => {
+        let [span,repeat] = [1,1];
+        
+        arr.forEach(item => {
+          Object.assign(item, axes.find(x => x.id === item.id));  
+          item.repeat = repeat;
+          repeat *= item.labels.length;
+        });
+        [...arr].reverse().forEach(item => {
+          item.span = span;
+          span *= item.labels.length;
+        });
+
+        arr.total = span;
+      }
+
+      prepareHeader(m.axes, m.columns);
+      prepareHeader(m.axes, m.rows);
+      console.log(m);
+
+      const grid = [];
+      m.rows.forEach(r => {
+        const row = [];
+        m.columns.forEach(()=>{row.push({value: ''})})
+        row[row.length-1].value = r.name;
+        for(let i=0; i<r.repeat; i++) {
+          for(let j=0; j<r.labels.length; j++) {
+            for(let k=0; k<r.span; k++) {
+              row.push({value: r.labels[j].name});
+            }
+          }
+        }
+        grid.push(row);
+      });
+
+
+      for(let i=0;i<m.rows.length;i++) {
+        const r = m.rows[i];
+        const row = [];
+
+        if(i===0) {
+          row.push({value:'', colSpan:m.columns.length-1, rowSpan:m.rows.length});
+        }
+        row.push({value:r.name});
+        for(let i=0;i<r.repeat;i++) {
+          r.labels.forEach(label=>{row.push({value:label.name, colSpan:r.span})});
+        }
+        grid.push(row);
+      }
+      
+      const bar = [];
+      m.columns.forEach(c=>{
+        bar.push({value:m.axes.find(x=>x.id===c.id).name});
+      })
+      bar.push({value:'', colSpan:m.rows.total});
+      grid.push(bar);
+
+      for(let i=0;i<m.columns.total;i++){
+        const row = [];
+        m.columns.forEach(c=>{
+          let remainder = Math.floor(i/c.span)%c.labels.length
+          if(i%c.span === 0) {
+            row.push({value:c.labels[remainder].name, rowSpan:c.span});
+          }
+        }); 
+        for(let j=0;j<m.rows.total;j++) {
+          row.push({value:''});
+        }
+        grid.push(row);
+      }
+      
+      return grid;
+    }
+
+    super(props)
+    this.state = {
+      grid:buildViewModel(model)
+    }
+  }
+
+  someMethod() {
+    console.log("hello");
+  }
+
+  render () {
+    return (
+      <Datasheet
+        data={this.state.grid}
+        valueRenderer={(cell) => cell.value}
+        onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
+        onCellsChanged={changes => {
+          const grid = this.state.grid.map(row => [...row])
+          changes.forEach(({cell, row, col, value}) => {
+            grid[row][col] = {...grid[row][col], value}
+          })
+          this.setState({grid})
+        }}
+      />
+    )
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ComponentSheet from "./ComponentSheet"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const app = ReactDOM.render(<App ref={(x) => {window.ourComponent = x}} model = {{
+    axes: [{id:11, name:'期数', labels:[{id:1, name:'3'}, {id:2, name:'6'}, {id:3, name:'9'}, {id:4, name:'12'} ]},
+      {id:12, name:'测试组', labels:[{id:1, name:'A'}, {id:2, name:'B'} ]},
+      {id:13, name:'风险等级', labels:[{id:1, name:'RG0'}, {id:2, name:'RG1'}, {id:3, name:'RG2'}, {id:4, name:'RG3'}, {id:5, name:'RG4'}, {id:6, name:'RG5'} ]},
+      {id:14, name:'复贷', labels:[{id:1, name:'是'}, {id:2, name:'否'} ]}],
+    rows: [{id:11},{id:12}],
+    columns: [{id:13},{id:14}],
+    keys: [{id:1, name:'3'}, {id:2, name:'6'}],
+    cells: [{labels:[{axis:11, label:1}, {axis:12, label:1}, {axis:13, label:1}, {axis:14, label:1}], values: [{key:1, value:'hello'}, {key:2, value:123}]}, 
+      {labels:[{axis:11, label:1}, {axis:12, label:2}, {axis:13, label:1}, {axis:14, label:1}], values: [{key:1, value:'world'}, {key:2, value:246}]}]
+  }} />, document.getElementById('root'));
+
+  window.ourComponent.someMethod();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
